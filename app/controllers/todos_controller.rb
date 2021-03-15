b2f634fe01b130663c674a22def12c129d8e8071
JatinRanka <jatinranka123@gmail.com>
2021-03-15 17:07:15 +0530

    Added update, create methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,30 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.order(:id).map {|todo| todo.to_pleasant_string}.join("\n")
+    # render plain: Todo.order(:id).map {|todo| todo.to_pleasant_string}.join("\n")
+    render "index"
   end
 
   def show
     id = params[:id].to_i
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
+  end
+
+  def create
+    new_todo = {
+      :todo_text => params[:todo_text],
+      :due_date => params[:due_date]
+    }
+    puts new_todo
+    todo = Todo.add_task(new_todo)
+    render plain: todo.to_pleasant_string
+  end
+
+  def update
+    id = params[:id]
+    updated_todo = Todo.mark_as_complete!(id)
+    render plain: updated_todo.to_pleasant_string
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,24 @@ class Todo < ActiveRecord::Base
 
   def to_pleasant_string
     is_completed = completed ? "[x]" : "[ ]"
-    return "#{id}. #{due_date.to_s(:short)}  #{todo_text} #{is_completed} "
+    return "#{id}. #{due_date.to_s(:short)} - #{todo_text} #{is_completed} "
   end
+
+  def self.add_task(new_todo)
+    Todo.create!(
+      "todo_text": new_todo[:todo_text],
+      "due_date": Date.today + new_todo[:due_in_days].to_i,
+      "completed": false
+    )
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    return todo
+  end
+
+
 
 end
