474cf9a7d90a4c5b83184c71ec59868b15a7984a
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-18 16:55:03 +0530

    Created Register new user feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  def new
+    render "index"
+  end
+
+  def create
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password]
+    )
+
+    redirect_to "/"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :todos
+  has_secure_password
 
 end
