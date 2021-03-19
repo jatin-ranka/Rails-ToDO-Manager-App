7fcf048c3b86e86c25478d03f5755baf6c4cc244
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-19 10:44:22 +0530

    Added sign-out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
+  delete "/signin" => "sessions#destroy", as: :destroy_session
 end
